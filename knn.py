3324c6000fb08055b6f0e39007f8f21d1338bc4a
Simona Stoyanoska <simonastoyanoska@student-209-76.eduroam.uu.se>
2025-02-18 19:04:57 +0100

    Skapat kNN fil, lags kNN figurer

---
+++ knn.py
@@ -0,0 +1,166 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model as skl_lm
+import sklearn.preprocessing as pp
+import sklearn.metrics as skl_m
+
+import sklearn.neighbors as skl_nb
+
+df = pd.read_csv('training_data_vt2025.csv')
+#df.info()
+
+# Modify the dataset, emphasizing different variables
+#df.iloc[:,12]=df.iloc[:,12]**2
+#df.iloc[:,13]=np.sqrt(df.iloc[:,13])
+#df.iloc[:,11] = df.iloc[:,11]**2
+
+df['month_cos'] = np.cos(df.month*np.pi/12)
+df['month_sin'] = np.sin(df.month*np.pi/12)
+
+# time of day, replaed with low,medium and high demand, 
+# adding the new categories back in the end.
+def categorize_demand(hour):
+    if 20 <= hour or 7 >= hour:
+        return 'night'
+    elif 8 <= hour <= 14:
+        return 'day'
+    elif 15 <= hour <= 19:
+        return 'evening'
+
+df['demand_category'] = df['hour_of_day'].apply(categorize_demand)
+df_dummies = pd.get_dummies(df['demand_category'], prefix='demand', drop_first=False)
+df = pd.concat([df, df_dummies], axis=1)
+
+# converting to bools
+def if_zero(data):
+    if data == 0:
+        return True
+    else:
+        return False
+
+# temperature
+
+df['snowdepth_bool'] = df['snowdepth'].replace(0, False).astype(bool)
+df['precip_bool'] = df['precip'].replace(0, False).astype(bool)
+
+# Split into train and test:
+
+#df.iloc[:,15]=df.iloc[:,15].replace('low_bike_demand',False)
+#df.iloc[:,15]=df.iloc[:,15].replace('high_bike_demand',True)
+np.random.seed(0)
+
+df_modified=df[[#'holiday',
+                'weekday',
+                #'summertime',
+                'temp',
+                #'dew',
+                'humidity',
+                'visibility',
+                'windspeed',
+                'month_cos',
+                'month_sin',
+                'demand_day',
+                'demand_evening',
+                'demand_night',
+                'snowdepth_bool',
+                'precip_bool',
+                'increase_stock']]
+
+N = df_modified.shape[0]
+n = round(0.7*N)
+trainI = np.random.choice(N,size=n,replace=False)
+trainIndex = df_modified.index.isin(trainI)
+train = df_modified.iloc[trainIndex]
+test = df_modified.iloc[~trainIndex]
+
+# Set up X,Y
+
+# Train data 
+X = train.iloc[:,0:-2]
+Y = train['increase_stock']
+
+# Test data
+X_test = test.iloc[:,0:-2]
+Y_test = test['increase_stock']
+
+
+"""
+# Tests for k-value
+# TEST 1 - uniform distance
+missclassification = []
+for k in range(500): # Try n_neighbours = 1, 2, ....,
+
+    #kNN method
+    scaler = pp.StandardScaler().fit(X)
+    model = skl_nb.KNeighborsClassifier(n_neighbors = k+1, weights = 'uniform')
+    model.fit(scaler.transform(X),Y)
+
+    # Prediction
+    y_hat = model.predict(scaler.transform(X_test))
+    missclassification.append(np.mean(y_hat != Y_test))
+
+K = np.linspace(1, 500, 500)
+plt.plot(K, missclassification, '.')
+plt.ylabel('Missclassification')
+plt.xlabel('Number of neighbours')
+plt.show()
+
+#TEST 2
+missclassification = []
+for k in range(500): # Try n_neighbours = 1, 2, ....,
+
+    #kNN method
+    scaler = pp.StandardScaler().fit(X)
+    model = skl_nb.KNeighborsClassifier(n_neighbors = k+1, weights = 'distance')
+    model.fit(scaler.transform(X),Y)
+
+    # Prediction
+    y_hat = model.predict(scaler.transform(X_test))
+    missclassification.append(np.mean(y_hat != Y_test))
+
+K = np.linspace(1, 500, 500)
+plt.plot(K, missclassification, '.')
+plt.ylabel('Missclassification')
+plt.xlabel('Number of neighbours')
+plt.show()
+"""
+
+
+
+# creating the model
+model = skl_nb.KNeighborsClassifier(n_neighbors = 120, weights = 'distance')
+
+
+# Scaling the data, otherwise
+scaler = pp.StandardScaler().fit(X)
+model.fit(scaler.transform(X),Y)
+y_hat = model.predict(scaler.transform(X_test))
+
+
+
+'''
+# oskalad data
+model.fit(X,Y)
+y_hat = model.predict(X_test)'''
+
+# Get confusion matrix
+diff = pd.crosstab(y_hat, Y_test)
+print(f'Confusion matrix: \n {diff}')
+
+# No. of TP,TN,FP,FN
+'''TP = diff.iloc[0,0]
+TN = diff.iloc[1,1]
+FP = diff.iloc[1,0]
+FN = diff.iloc[0,1]'''
+
+# Get metrics:
+print(skl_m.classification_report(Y_test, y_hat))
+
+
+
+
+
+
+
+
