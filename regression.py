f12afdd8475a1d9d21eaba858dee88fa6010ed49
Erik Björk <erik.bjork13@gmail.com>
2025-02-20 12:04:32 +0100

    ändrade lite i datahanteringen, kan ändra i restsen om vi får feedback på det typ

--- regression.py
+++ regression.py
@@ -13,8 +13,8 @@ df = pd.read_csv('training_data_vt2025.csv')
 #df.iloc[:,13]=np.sqrt(df.iloc[:,13])
 #df.iloc[:,11] = df.iloc[:,11]**2
 
-df['month_cos'] = np.cos(df.month*np.pi/12)
-df['month_sin'] = np.sin(df.month*np.pi/12)
+df['month_cos'] = np.cos(df.month*2*np.pi/12) # period of 12 months
+df['month_sin'] = np.sin(df.month*2*np.pi/12)
 
 # time of day, replaed with low,medium and high demand, 
 # adding the new categories back in the end.
@@ -26,28 +26,20 @@ def categorize_demand(hour):
     elif 15 <= hour <= 19:
         return 'evening'
 
+# Adding the categories back, but creating three new categories 
+# for the different times
 df['demand_category'] = df['hour_of_day'].apply(categorize_demand)
 df_dummies = pd.get_dummies(df['demand_category'], prefix='demand', drop_first=False)
 df = pd.concat([df, df_dummies], axis=1)
 
 # converting to bools
-def if_zero(data):
-    if data == 0:
-        return True
-    else:
-        return False
-
-# temperature
-
 df['snowdepth_bool'] = df['snowdepth'].replace(0, False).astype(bool)
 df['precip_bool'] = df['precip'].replace(0, False).astype(bool)
 
 # Split into train and test:
-
-#df.iloc[:,15]=df.iloc[:,15].replace('low_bike_demand',False)
-#df.iloc[:,15]=df.iloc[:,15].replace('high_bike_demand',True)
 np.random.seed(0)
 
+# Can try different combinations, which inputs give worse perf etc
 df_modified=df[[#'holiday',
                 'weekday',
                 #'summertime',
